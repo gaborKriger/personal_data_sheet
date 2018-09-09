5174656ebe8ff89d5d3fb8309549f7a233a65778
gaborKriger <regirck@gmail.com>
2018-09-09 15:17:27 +0200

    show full name

--- server.py
+++ server.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
+    return render_template('index.html')
+
+@app.route('/show-all-full-name')
+def full_name():
     full_name = data_manager.get_full_name()
-    return render_template('index.html', full_name=full_name)
+    return render_template('result.html', full_name=full_name)
 
 
 if __name__ == '__main__':
